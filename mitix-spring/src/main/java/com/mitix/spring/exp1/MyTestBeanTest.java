d1e4912e98de0d64de4d3771a4c3cb1d46db5eb7
infoconsole <hlh19850220@163.com>
2016-11-25 15:59:59 +0800

    spring-len

---
+++ mitix-spring/src/main/java/com/mitix/spring/exp1/MyTestBeanTest.java
@@ -0,0 +1,17 @@
+package com.mitix.spring.exp1;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+@SuppressWarnings("deprecation")
+public class MyTestBeanTest {
+
+	public static void main(String[] args) {
+		//测试第一个Beans实例
+		BeanFactory factory = new XmlBeanFactory(new ClassPathResource("exp1/applicationContext.xml"));
+	    MyTestBean bean=(MyTestBean) factory.getBean("mytestbean");
+	    System.out.println(bean.getTestStr());
+	}
+
+}
